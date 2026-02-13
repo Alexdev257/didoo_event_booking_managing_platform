d37788eb85bd011a4ed37014cfa945f6146447b0
Alexdev257 <buiphuocthang2507@gmail.com>
2026-02-14 04:00:21 +0700

    init all resale service, fix apigateway about connect to resaleservice

---
+++ BE/EventManagement/services/ResaleService/src/ResaleService.Domain/Enum/ResaleTransactionStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResaleService.Domain.Enum
+{
+    public enum ResaleTransactionStatusEnum
+    {
+        Pending = 1,
+        Completed = 2,
+        Failed = 3,
+        Refunded = 4
+    }
+}
---
+++ BE/EventManagement/services/ResaleService/src/ResaleService.Infrastructure/Migrations/20260213204249_InitResaleService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResaleService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitResaleService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Resales",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SalerUserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    booking_detail_id = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    description = table.Column<string>(type: "varchar(2000)", maxLength: 2000, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: true),
+                    status = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    created_by = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    is_deleted = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: false),
+                    deleted_at = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resales", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ResaleTransactions",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    resale_id = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    buyer_user_id = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    cost = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    fee_cost = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    status = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    transaction_date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    created_by = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    is_deleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResaleTransactions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ResaleTransactions_Resales_resale_id",
+                        column: x => x.resale_id,
+                        principalTable: "Resales",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resales_booking_detail_id",
+                table: "Resales",
+                column: "booking_detail_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resales_SalerUserId",
+                table: "Resales",
+                column: "SalerUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resales_status",
+                table: "Resales",
+                column: "status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResaleTransactions_buyer_user_id",
+                table: "ResaleTransactions",
+                column: "buyer_user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResaleTransactions_resale_id",
+                table: "ResaleTransactions",
+                column: "resale_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResaleTransactions_status",
+                table: "ResaleTransactions",
+                column: "status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ResaleTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Resales");
+        }
+    }
+}
