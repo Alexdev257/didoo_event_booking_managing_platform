440e782f600be99d7998c3b4e68bbf831b057fa6
Alexdev257 <buiphuocthang2507@gmail.com>
2026-03-14 10:04:16 +0700

    update signalr

--- BE/EventManagement/services/OperationService/src/OperationService.Infrastructure/DependencyInjection/ManageDependencyInjection.cs
+++ BE/EventManagement/services/OperationService/src/OperationService.Infrastructure/DependencyInjection/ManageDependencyInjection.cs
@@ -104,6 +104,16 @@ namespace OperationService.Infrastructure.DependencyInjection
 
                     options.Events = new JwtBearerEvents
                     {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        },
                         OnAuthenticationFailed = context =>
                         {
                             if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
